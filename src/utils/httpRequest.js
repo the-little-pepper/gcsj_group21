8621c89e8a29da43eceff9ceb09fbe0c7f2821e0
unknown <571472870@qq.com>
2021-04-05 18:24:40 +0800

    add http request file and update an inceptor

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -41,14 +41,13 @@ http.interceptors.response.use(response => {
  * @param {*} actionName action方法名称
  */
 http.adornUrl = (actionName) => {
-  // 非生产环境 && 开启代理, 接口前缀统一使用[/proxyApi/]前缀做代理拦截!
   return (process.env.NODE_ENV !== 'production' && process.env.OPEN_PROXY ? '/proxyApi/' : window.SITE_CONFIG.baseUrl) + actionName
 }
 
 /**
  * get请求参数处理
  * @param {*} params 参数对象
- * @param {*} openDefultParams 是否开启默认参数?
+ * @param {*} openDefultParams 是否开启默认参数
  */
 http.adornParams = (params = {}, openDefultParams = true) => {
   var defaults = {
